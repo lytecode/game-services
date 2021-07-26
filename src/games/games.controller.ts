0380a3895e20aaae7934167f6dc0ea6ab497af47
lytecode <mbonubasil@gmail.com>
2021-07-26 09:19:03 +0100

    Add update and get publisher endpoints

--- src/games/games.controller.ts
+++ src/games/games.controller.ts
@@ -1,12 +1,12 @@
 import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { CreateGameDto } from './dto/create-game.dto';
-// import { UpdateUserDto } from './dto/update-user.dto';
+import { UpdateGameDto } from './dto/update-game.dto';
 
 import { Game } from './schemas/game.schema';
 import { GamesService } from './games.service';
 import { ApiTags } from '@nestjs/swagger';
 @ApiTags('games')
-@Controller('/games')
+@Controller('/api/v1/games')
 export class GamesController {
   constructor(private readonly gamesService: GamesService) {}
 
@@ -25,5 +25,14 @@ export class GamesController {
     return this.gamesService.createGame(createGameDto)
   }
 
+  @Patch(':id')
+  async updateUser(@Param('id') gameId: string, @Body() updateGameDto: UpdateGameDto): Promise<Game> {
+    return this.gamesService.updateGame(gameId, updateGameDto);
+  }
+
+  @Get(':id/publisher')
+  async getPublisher(@Param('id') gameId: string): Promise<{id: number, name: string, siret: number, phone: string}> {
+    return this.gamesService.getPublisher(gameId)
+  }
 
 }
--- src/games/games.service.ts
+++ src/games/games.service.ts
@@ -4,6 +4,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Game, GameDocument } from './schemas/game.schema';
 import { CreateGameDto } from './dto/create-game.dto';
 import { BadRequestException } from '@nestjs/common';
+import { UpdateGameDto } from './dto/update-game.dto';
 
 @Injectable()
 export class GamesService {
@@ -26,6 +27,38 @@ export class GamesService {
   }
 
   async getGames(): Promise<Game[]> {
-    return this.gameModel.find().exec();
+    return await this.gameModel.find().exec();
+  }
+
+  async updateGame(gameId: string, gameUpdates: UpdateGameDto): Promise<Game> {
+    const { title, price, tags, releaseDate, publisher } = gameUpdates;
+    
+    let updateGame;
+    try {
+      updateGame = await this.gameModel.findById(gameId);
+    }catch (error) {
+      throw new NotFoundException('No game found');
+    }
+
+    if (title) updateGame.title = title;
+    if (price) updateGame.price = price;
+    if (tags) updateGame.tags = tags;
+    if (releaseDate) updateGame.releaseDate = releaseDate;
+    if(publisher?.id) updateGame.publisher.id = publisher.id;
+    if(publisher?.name) updateGame.publisher.id = publisher.name;
+    if(publisher?.siret) updateGame.publisher.id = publisher.siret;
+    if(publisher?.phone) updateGame.publisher.id = publisher.phone;
+
+    return updateGame.save();
+  }
+
+  async getPublisher(gameId: string): Promise<{id: number, name: string, siret: number, phone: string}>{
+    try {
+      const { publisher } = await this.gameModel.findById(gameId);
+      return publisher
+    } catch (error) {
+      throw new BadRequestException('Cannot find game or publisher with the given id')
+    }
+    
   }
 }
