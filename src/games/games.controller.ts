4a3d7b93f8b7fc9a6997ad39a7fb60c3108cc72a
lytecode <mbonubasil@gmail.com>
2021-07-26 06:14:25 +0100

    Add routes to get single game, create game and fetch all games

---
+++ src/games/games.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { CreateGameDto } from './dto/create-game.dto';
+// import { UpdateUserDto } from './dto/update-user.dto';
+
+import { Game } from './schemas/game.schema';
+import { GamesService } from './games.service';
+
+@Controller('/games')
+export class GamesController {
+  constructor(private readonly gamesService: GamesService) {}
+
+  @Get(':id')
+  async getGame(@Param('id') gameId: string): Promise<Game> {
+    return this.gamesService.getGameById(gameId);
+  }
+
+  @Get()
+  async getGames(): Promise<Game[]> {
+    return this.gamesService.getGames();
+  }
+
+  @Post()
+  async createGame(@Body() createGameDto: CreateGameDto): Promise<Game> {
+    return this.gamesService.createGame(createGameDto)
+  }
+
+
+}
