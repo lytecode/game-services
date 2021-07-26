3e51e5c0b4190a90582ad4361f8cbe394c957989
lytecode <mbonubasil@gmail.com>
2021-07-26 06:49:37 +0100

    Add swagger documentation setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Games services')
+    .setDescription('The games APIs')
+    .setVersion('1.0')
+    .addTag('games')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1', app, document);
+  
   await app.listen(5000);
 }
 bootstrap();
