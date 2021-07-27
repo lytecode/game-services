8c7655cc9e5c9376498de863e84c507e539848dc
lytecode <mbonubasil@gmail.com>
2021-07-27 22:49:15 +0100

    Annotate with class-validator

--- src/games/dto/create-game.dto.ts
+++ src/games/dto/create-game.dto.ts
@@ -1,29 +1,42 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { ArrayMinSize, IsArray, IsDate, IsNumber, IsString } from "class-validator";
 
-class PublisherProperties {
-    @ApiProperty()
+class Publisher {
+    @ApiProperty({ description: "id", required: true })
+    @IsNumber()
     id: number;
-    @ApiProperty()
+
+    @ApiProperty({ description: "name", required: true })
+    @IsString()
     name: string;
-    @ApiProperty()
+
+    @ApiProperty({ description: "siret", required: true })
+    @IsNumber()
     siret: number
-    @ApiProperty()
+
+    @ApiProperty({ description: "phone", required: true })
+    @IsString()
     phone: string
 }
 export class CreateGameDto {
-    @ApiProperty()
+    @ApiProperty({ description: 'title', required: true})
+    @IsString()
     title: string;
     
-    @ApiProperty()
+    @ApiProperty({ description: 'price', required: true})
+    @IsNumber()
     price: number;
 
-    @ApiProperty()
+    @ApiProperty({ description: 'tags', required: true})
+    @IsArray()
+    @ArrayMinSize(1)
     tags: string[];
 
-    @ApiProperty()
+    @ApiProperty({ description: 'releaseDate', required: true})
+    @IsDate()
     releaseDate: Date;
 
     @ApiProperty()
-    publisher: PublisherProperties
+    publisher: Publisher
 }
 
